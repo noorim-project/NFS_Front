7aba960c3ec36720303d986bd3a64a4e5cc01a51
Kim Jun Woo <junukim.dev@gmail.com>
2019-04-11 11:41:53 +0900

    UPDATE: webpack setting

---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ["@babel/polyfill", __dirname + "/src/index.js"],
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin, "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              name: "[name].[ext]?[hash]",
+              publicPath: "/dist",
+              limit: 100000
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+    alias: {
+      "react-dom": "@hot-loader/react-dom"
+    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html",
+      showErrors: true
+    }),
+    new MiniCssExtractPlugin({
+      template: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  devtool: "inline-source-map"
+};
