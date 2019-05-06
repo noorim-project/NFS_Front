fdbb18a2838d2ddb7ee4534eb022be47ee575d20
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-06 23:49:24 +0900

    UPDATE: lading TeamText style

--- src/styles/main/index.js
+++ src/styles/main/index.js
@@ -1,6 +1,23 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const MainContainer = styled.div`
-  width: 100%;
-  min-width: 1280px;
+export const LandingTeamText = styled.h1`
+  font-family: Lato;
+  font-size: 144px;
+  line-height: 1.2;
+  text-align: left;
+  /* color: #ffffff; */
+  animation: ${LandingTeamTextAnimation} 1.5s ease-in-out delay iteration-count direction fill-mode;
+`;
+
+const LandingTeamTextAnimation = keyframes`
+  0% {
+    display: none;
+
+  }
+
+
+
+  100% {
+
+  }
 `;
