094f928ef0cd4383634e2ca6b412fad9d8ad7b0e
Kim Jun Woo <junukim.dev@gmail.com>
2019-05-08 15:30:08 +0900

    UPDATE: LandingPage views

--- src/components/Main/Landing.jsx
+++ src/components/Main/Landing.jsx
@@ -6,15 +6,31 @@ import {
   LandingContents,
   LadingSubtitleText,
   LandingComponent,
+  LandingItems,
+  LandingMessage,
+  LandingButtonCover,
 } from '../../styles/main';
+import LandingButton from './LandingButton';
 
 const Landing = () => (
   <LandingComponent>
     <Component>
-      <LandingTeamText>NFS</LandingTeamText>
-      <LandingContents>
-        <LadingSubtitleText>Noorim For Students</LadingSubtitleText>
-      </LandingContents>
+      <LandingItems>
+        <LandingTeamText>NFS</LandingTeamText>
+        <LandingContents>
+          <LadingSubtitleText>Noorim For Students</LadingSubtitleText>
+          <LandingMessage>
+            학생들의
+            <br />
+            편리함을 추구합니다
+          </LandingMessage>
+          <LandingButtonCover>
+            <LandingButton pathURL="/login" content="로그인" />
+            <div className="filter" />
+            <LandingButton pathURL="/register" content="회원가입" />
+          </LandingButtonCover>
+        </LandingContents>
+      </LandingItems>
     </Component>
   </LandingComponent>
 );
