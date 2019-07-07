e5629d9307604fa1d91d081f7aedbf967c2f82a2
sema0710 <pandati@naver.com>
2019-07-07 22:46:32 +0900

    Main_header
    Main header를 만듬
    Main styled-components file을 만듬

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 
 import { Component } from '../../styles/gloobalStlye';
 import { MainComponent } from '../../styles/main';
-
+import  { Header_div }  from './Main_Header_content';
+import { Header_Content } from './Main_Header_content';
 const Main = () => (
-  <MainComponent>
-    <Component />
-  </MainComponent>
+    <MainComponent>
+      <Component>
+        <Header_div>
+          <Header_Content href="#">공지사항</Header_Content>
+          <Header_Content href="#">건의사항</Header_Content>
+          <Header_Content href="#">캘린더</Header_Content>
+          <Header_Content href="#">시간표</Header_Content>
+          <Header_Content href="#">선생님 자리</Header_Content>
+          <Header_Content href="#">외출신청</Header_Content>
+          <Header_Content href="#">운동신청</Header_Content>
+          <Header_Content href="#">전공 동아리</Header_Content>
+        </Header_div>
+      </Component>
+    </MainComponent>
 );
 
 export default Main;
---
+++ src/components/Main/Main_Header_content.js
@@ -0,0 +1,22 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const Header_Content = styled.a`
+  color:white;
+  font-size:18px;
+  width:82px;
+  height:20px;
+  text-align:center;
+  margin-left:22px;
+  :link{
+    color:white;
+  }
+  :visited{
+    color:white;
+  }
+`
+
+export const Header_div = styled.div`
+  position:absolute;
+  margin-left:550px;
+  margin-top:58px;
+`
