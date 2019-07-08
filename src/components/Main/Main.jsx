97cf84d07630e0b58ead005603ac465794f6ec61
sema0710 <pandati@naver.com>
2019-07-08 15:44:46 +0900

    add design and 공지사항

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import { Component } from '../../styles/gloobalStlye';
 import { MainComponent } from '../../styles/main';
-import  { Header_div }  from './Main_Header_content';
+import  { Header_div, Design_text }  from './Main_Header_content';
 import { Header_Content } from './Main_Header_content';
+import { Content_div } from './Main_Header_content';
+import { Design_div } from './Main_Header_content';
+import { Content_title } from './Main_Header_content';
 const Main = () => (
     <MainComponent>
       <Component>
@@ -17,6 +20,14 @@ const Main = () => (
           <Header_Content href="#">운동신청</Header_Content>
           <Header_Content href="#">전공 동아리</Header_Content>
         </Header_div>
+
+          <Design_div/>
+          <Design_text>학생들의 편리함을 추구합니다.</Design_text>
+
+        <Content_div>
+          <Content_title>공지사항</Content_title>
+        </Content_div>
+
       </Component>
     </MainComponent>
 );
--- src/components/Main/Main_Header_content.js
+++ src/components/Main/Main_Header_content.js
@@ -3,7 +3,7 @@ import styled, { createGlobalStyle } from 'styled-components';
 export const Header_Content = styled.a`
   color:white;
   font-size:18px;
-  width:82px;
+  width:5%;
   height:20px;
   text-align:center;
   margin-left:22px;
@@ -16,7 +16,48 @@ export const Header_Content = styled.a`
 `
 
 export const Header_div = styled.div`
+  margin-left:42%;
+  margin-top:7vh;
+  
+`
+export const Design_div = styled.div`
+  width:2.5%;
+  height:160px;
+  background-color:#ff2f6f;
+  top:36%;
   position:absolute;
-  margin-left:550px;
-  margin-top:58px;
+`
+
+export const Design_text = styled.p`
+  font-family: NanumSquareEB;
+  font-size: 40px;
+  line-height: 1.7;
+  text-align: left;
+  color: #ffffff;
+  max-width:30%;
+  font-weight:bold;
+  position:absolute;
+  left:5%;
+  top:36.5%;
+`
+
+export const Content_div = styled.div`
+  width:100%;   
+  height:100vh;
+  top:100%;
+  background-color:white;
+  position:absolute;
+`
+
+export const Content_title = styled.p`
+  font-family: NanumSquareB;
+  font-size: 50px;
+  text-align: center;
+  color: #24252f;
+  margin-top: 9%;
+  font-weight:bold;
+`
+
+export const Content_table = styled.table`
+  width:80%;
 `
