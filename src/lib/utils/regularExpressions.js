0bdd9c3d9eacce00d73ca6344fb71b9aad4e704d
jjunu <junukim.dev@gmail.com>
2019-07-09 23:36:46 +0900

    Create regularExpressions.js

---
+++ src/lib/utils/regularExpressions.js
@@ -0,0 +1,3 @@
+export const emailRegular = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+export const passwordRegulat = /^[a-zA-Z0-9]{8,15}$/;
+export const authenticationNumber = /^[0-9]{6}$/;
