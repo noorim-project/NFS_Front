fd6f43fe96be8349302a9188fd95f6fbc61a000c
sema0710 <pandati@naver.com>
2019-07-15 20:06:00 +0900

    옛날에 적어놓고 안쓰는 props를 정리했다

--- src/components/Main/TimeTableContent.jsx
+++ src/components/Main/TimeTableContent.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 import * as S from './Main_content'
 
-const TimeTableContent = ({Mon,Tue,Wen,Thu,Fri,wid,num})=>{
+const TimeTableContent = ({Mon,Tue,Wen,Thu,Fri,num})=>{
   return(
     <S.Content_tr>
-      <S.TimeTableTd wid={wid} padding="1" >{num}</S.TimeTableTd>
-      <S.TimeTableTd wid={wid}>{Mon}</S.TimeTableTd>
-      <S.TimeTableTd wid={wid}>{Tue}</S.TimeTableTd>
-      <S.TimeTableTd wid="1" padding="1">{Wen}</S.TimeTableTd>
-      <S.TimeTableTd wid={wid}>{Thu}</S.TimeTableTd>
-      <S.TimeTableTd wid={wid}>{Fri}</S.TimeTableTd>
+      <S.TimeTableTd width="10px" minwidth="1">{num}</S.TimeTableTd>
+      <S.TimeTableTd>{Mon}</S.TimeTableTd>
+      <S.TimeTableTd>{Tue}</S.TimeTableTd>
+      <S.TimeTableTd padding="1">{Wen}</S.TimeTableTd>
+      <S.TimeTableTd>{Thu}</S.TimeTableTd>
+      <S.TimeTableTd>{Fri}</S.TimeTableTd>
     </S.Content_tr>
   )
 }
