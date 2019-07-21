f95e4e94fdbe95d50b27729a8550feb6cebf4f8f
sema0710 <pandati@naver.com>
2019-07-21 21:50:48 +0900

    footer의 위치와 크기를 줄였을 때 깨지는 버그 수정

--- src/components/Main/MainFooter.jsx
+++ src/components/Main/MainFooter.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
 import * as S from './Main_content'
-import { Cloud , School , Facebook } from '../../assets/Main'
+import { Cloud , School , Facebook , DMS} from '../../assets/Main'
 
 const MainFooter = ()=>{
     return(
         <S.MainFooterDiv>
             <div>
-                <S.MainFooterButton><S.MainFooterButtonImg src={Facebook}/></S.MainFooterButton>
-                <S.MainFooterButton><S.MainFooterButtonImg src={School}/></S.MainFooterButton>
-                <S.MainFooterButton><S.MainFooterButtonImg src={Cloud}/></S.MainFooterButton>
-                <S.MainFooterButton><S.MainFooterButtonImg src=""/></S.MainFooterButton>
-                <S.MainFooterButton><S.MainFooterButtonImg src=""/></S.MainFooterButton>
+                <S.MainFooterButton><S.MainFooterButtonImg src={Facebook}/><S.MainFooterButtonSpan>Noorim Facebook</S.MainFooterButtonSpan></S.MainFooterButton>
+                <S.MainFooterButton><S.MainFooterButtonImg src=""/><S.MainFooterButtonSpan>Entry</S.MainFooterButtonSpan></S.MainFooterButton>
+                <S.MainFooterButton><S.MainFooterButtonImg src={Cloud}/><S.MainFooterButtonSpan>Cloud</S.MainFooterButtonSpan></S.MainFooterButton>
+                <S.MainFooterButton><S.MainFooterButtonImg src={School}/><S.MainFooterButtonSpan>School</S.MainFooterButtonSpan></S.MainFooterButton>
+                <S.MainFooterButton><S.MainFooterButtonImg src={DMS}/><S.MainFooterButtonSpan>DMS</S.MainFooterButtonSpan></S.MainFooterButton>
             </div>
         </S.MainFooterDiv>
     )
